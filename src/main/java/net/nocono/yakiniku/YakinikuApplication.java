0260d50191b363980bb7d929542825dc4fa2300e
Abe Asami <sammy@nocono.net>
2022-12-21 20:08:26 +0900

    肉を焼いていくぅー

---
+++ src/main/java/net/nocono/yakiniku/YakinikuApplication.java
@@ -0,0 +1,13 @@
+package net.nocono.yakiniku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakinikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakinikuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/nocono/yakiniku/YakinikuApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nocono.yakiniku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakinikuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
